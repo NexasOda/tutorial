916720b75329ed7acebb562feee292a36344fc07
NexasIshizuka <chisato.ishizuka@nexaswing.co.jp>
2020-05-12 13:14:43 +0900

    機能改修2

--- question02/Kazuate.java
+++ question02/Kazuate.java
@@ -21,34 +21,43 @@ import java.util.Random;
  *
  */
 
-
 public class Kazuate {
 	public static void main(String[] args) {
 		Random random = new Random();
 
-		int ans = random.nextInt(1000);
-		int count = 0;
-		int userAns = 0;
+		int playAgain = 1;
 
-		System.out.println("0～999の数字を入力してください。");
+		while (playAgain == 1) {
+			int ans = random.nextInt(1000);
+			int count = 0;
+			int userAns = 0;
 
-		while (count < 10) {
-			System.out.printf("%d> ", count + 1);
-			userAns = getNumberInput();
-			System.out.printf("\t%d ..... ", userAns);
-			if (ans == userAns) {
-				System.out.println("正解！");
-				break;
-			} else if (ans < userAns) {
-				System.out.println("大きすぎます。");
-			} else {
-				System.out.println("小さすぎます。");
+			System.out.println("0～999の数字を入力してください。");
+
+			while (count < 10) {
+				System.out.printf("%d> ", count + 1);
+				userAns = getNumberInput();
+				System.out.printf("\t%d ..... ", userAns);
+				if (ans == userAns) {
+					System.out.println("正解！");
+					break;
+				} else if (ans < userAns) {
+					System.out.println("大きすぎます。");
+				} else {
+					System.out.println("小さすぎます。");
+				}
+				count++;
 			}
-			count++;
-		}
 
-		if (ans != userAns) {
-			System.out.println("Game Over! 正解は " + ans + " でした。");
+			if (ans != userAns) {
+				System.out.println("Game Over! 正解は " + ans + " でした。");
+			}
+
+			System.out.println("もう一度遊びますか？ Yes:1 / No:1以外");
+			System.out.print(">");
+			String playAgainS = getStringInput();
+			playAgain = Integer.parseInt(playAgainS);
+
 		}
 	}
 
